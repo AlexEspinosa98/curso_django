60a3b030a672f2362d9c4deac95fc816add451a3
AlexEspinosa98 <espinosaalexander09@gmail.com>
2023-08-17 22:17:38 -0500

    Eliminación logica y total

--- biblioteca/libros/urls.py
+++ biblioteca/libros/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
-from .views import Home,crearAutor
+from django.urls import path,re_path
+from .views import Home,crearAutor,listarAutor,editarAutor,eliminarAutor
 
 urlpatterns = [
     path('',Home,name='index'),
     #path('crear_autor/',crearautor, name='crear_autor')
-    path('crear_autor/', crearAutor,name='crear_autor')
+    path('crear_autor/', crearAutor,name='crear_autor'),
+    path('listar_autor/',listarAutor,name='listar_autor'),
+    path('editar_autor/<int:id>',editarAutor, name = 'editar_autor'),
+    path('eliminar_autor/<int:id>',eliminarAutor, name = 'eliminar_autor'),
+    #re_path
+   
 ]
--- biblioteca/libros/views.py
+++ biblioteca/libros/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import AutorForm
+from .models import Autor
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def Home(request):
@@ -14,4 +16,36 @@ def crearAutor(request):
     else:
         autor_form= AutorForm()
     return render(request,'libros/crear_autor.html',{'autor_form':autor_form})
-            
+
+def listarAutor(request):
+    autores=Autor.objects.filter(estado=True)
+    return render(request,'libros/listar_autor.html',{'autores':autores})
+
+def editarAutor(request,id):
+    autor_form=None
+    error=None
+    try:
+        autor=Autor.objects.get(id = id)
+        if request.method=='GET':
+            autor_form=AutorForm(instance=autor)
+        else:
+            autor_form=AutorForm(request.POST,instance=autor)
+            if autor_form.is_valid():
+                autor_form.save()
+            return redirect('libro:listar_autor')
+       
+    except ObjectDoesNotExist as e:
+        error = e 
+    
+    return render(request,'libros/crear_autor.html',{'autor_form':autor_form, 'error':error})
+                
+def eliminarAutor(request,id):
+    autor=Autor.objects.get(id=id)
+    if request.method== 'POST':
+        
+        #autor.delete() eliminación base de datos
+        # Eliminación logica
+        autor.estado=False
+        autor.save()
+        return redirect('libro:listar_autor')
+    return render(request,'libros/eliminar_autor.html',{'autor':autor})
